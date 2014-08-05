a969a29c9244ce28b1c9cd5b22978966aa618b98
Jesse Wolgamott <jesse@comalproductions.com>
2014-08-05 11:11:22 -0500

    move to unicorn directory

---
+++ config/unicorn/production.rb
@@ -0,0 +1,8 @@
+working_directory "/home/deploy/apps/beerme"
+pid "/home/deploy/apps/beerme/tmp/pids/unicorn.pid"
+stderr_path "/home/deploy/apps/beerme/log/unicorn.log"
+stdout_path "/home/deploy/apps/beerme/log/unicorn.log"
+
+listen "/tmp/unicorn.shop.sock"
+worker_processes 2
+timeout 30
